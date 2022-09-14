56d72ba458e7f206450c9d7008b035147fce7a8c
Ayaz <ayazmansuri.techie@gmail.com>
2022-09-14 21:59:28 +0530

    added file

--- src/main/java/com/employee/details/SpringbootGithubActionApplication.java
+++ src/main/java/com/employee/details/SpringbootGithubActionApplication.java
@@ -2,6 +2,7 @@ package com.employee.details;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ public class SpringbootGithubActionApplication {
 		SpringApplication.run(SpringbootGithubActionApplication.class, args);
 	}
 	
+	@GetMapping("/details")
 	public void add() {
 		
 		System.out.println("Welcome to Git Hub action program");
